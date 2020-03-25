fc2ce289a769be36f6e27a6a627a0066589958aa
Diegw <diego.wechselberg@gmail.com>
2020-03-25 00:50:14 -0300

    add: player and enemy prototype

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float _movementSpeed = 0f;
+    [SerializeField] private float _rotationSpeed = 0f;
+    [SerializeField] private float _increaseScaleSize = 0f;
+    [SerializeField] private GameObject _target = null;
+    private Vector3 _scaleSizeVector;
+
+    private void Start()
+    {
+        _scaleSizeVector = new Vector3(_increaseScaleSize, _increaseScaleSize);
+
+    }
+
+    private void OnTriggerStay2D()
+    {
+        Vector3 currentPosition = this.transform.position;
+        Vector3 targetPosition = _target.transform.position;
+        Vector3 directionToMove = targetPosition - currentPosition;
+        
+        //Debug.DrawRay(currentPosition, this.transform.up*5, Color.blue, 20);
+        //Debug.DrawRay(currentPosition, directionToMove, Color.red, 20);
+        
+        float angleBetweenEnemyAndTarget = Vector3.SignedAngle(this.transform.up, directionToMove, this.transform.forward);
+        this.transform.Rotate(0, 0 , (angleBetweenEnemyAndTarget * _rotationSpeed));
+
+        this.transform.Translate(this.transform.up * _movementSpeed * Time.deltaTime, Space.World);
+    }
+
+    // private void OnTriggerEnter2D(Collider2D objectCollideWith)
+    // {
+    //     Debug.Log("Collision Enter");
+    //     Transform transform = objectCollideWith.GetComponent<Transform>();
+    //     transform.localScale += scaleSizeVector;
+    //     Destroy(gameObject);
+    // }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,5 +2,10 @@
 
 public class Player : MonoBehaviour
 {
-    
+    private void OnMouseDrag()
+    {
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePosition.z = -1;
+        gameObject.transform.position = mousePosition;
+    }
 }
